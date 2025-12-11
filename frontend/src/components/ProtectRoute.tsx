805add6aeed1ffebb9c1e29c5bd7b2068cde3389
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-12-11 22:27:23 +0530

    fix ProtectRoute

--- frontend/src/components/ProtectRoute.tsx
+++ frontend/src/components/ProtectRoute.tsx
@@ -1,11 +1,14 @@
 import { useNavigate } from "react-router";
 import type { ProtectRoutePropTypes } from "../utils/types";
+import { useEffect } from "react";
 
 export default function ProtectRoute({ children }: ProtectRoutePropTypes) {
   const playerId = localStorage.getItem("playerId");
-  const navigate = useNavigate();
-  if (playerId === null) {
-    navigate("/register");
-  }
+  const navigate  = useNavigate()
+  useEffect(() => {
+    if(playerId === null) {
+      navigate("/register")
+    }
+  }, [playerId])
   return children;
 }
