52d8856fa8b6ce6d188526baa46593cb6c41bc6f
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-12-26 13:38:11 +0530

    feat: add functionality to close popups

--- frontend/src/components/GamePopup.tsx
+++ frontend/src/components/GamePopup.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import type { GamePopupPropTypes } from "../utils/types";
-import { useGetImagesToBeFound } from "../utils/hooks";
+import { useGetImagesToBeFound, useTheme, useScore } from "../utils/hooks";
 import ImageToBeFound from "./ImageToBeFound";
 import { checkPositions } from "../utils/checkPositions";
-import { useScore } from "../utils/hooks";
+import closeLightModeImage from "../assets/images/theme-images/close_light_mode.svg";
+import closeDarkModeImage from "../assets/images/theme-images/close_dark_mode.svg";
 
 export default function GamePopup({
   showGamePopup,
@@ -17,6 +18,7 @@ export default function GamePopup({
   const toFindImages = getImagesToBeFound(imageId);
   const { x, y } = mousePosition;
   const { incrementScore } = useScore();
+  const { theme } = useTheme();
   useEffect(() => {
     if (showGamePopup) {
       setTimeout(() => {
@@ -26,7 +28,21 @@ export default function GamePopup({
   }, []);
   return (
     <div className={`fixed right-3/7 top-1/9 border p-4 text-xl ${className}`}>
-      <p>Choose your image!</p>
+      <div className="flex flex-row gap-3 items-center mb-1">
+        <div
+          className="flex h-8 w-8 cursor-pointer"
+          onClick={() => {
+            setShowGamePopup(false);
+          }}
+        >
+          {theme === "light" ? (
+            <img src={closeDarkModeImage} alt="close-dark-mode-image" />
+          ) : (
+            <img src={closeLightModeImage} alt="close-light-mode-image" />
+          )}
+        </div>
+        <p>Choose your image!</p>
+      </div>
       <div className="flex gap-3">
         {toFindImages.map((src, index) => {
           return (
--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import type { PopupPropTypes } from "../utils/types";
+import { useTheme } from "../utils/hooks";
+import closeLightModeImage from "../assets/images/theme-images/close_light_mode.svg";
+import closeDarkModeImage from "../assets/images/theme-images/close_dark_mode.svg";
 
 export default function Popup({
   content,
@@ -14,11 +17,26 @@ export default function Popup({
       }, 10000);
     }
   }, []);
+  const { theme } = useTheme();
   return (
     <div
       className={`fixed right-1/10 bottom-1/7 border p-4 text-xl ${className}`}
     >
-      <p>{content}</p>
+      <div className="flex flex-row gap-3 items-center">
+        <div
+          className="flex h-8 w-8 cursor-pointer"
+          onClick={() => {
+            setShowPopup(false);
+          }}
+        >
+          {theme === "light" ? (
+            <img src={closeDarkModeImage} alt="close-dark-mode-image" />
+          ) : (
+            <img src={closeLightModeImage} alt="close-light-mode-image" />
+          )}
+        </div>
+        <p>{content}</p>
+      </div>
     </div>
   );
 }
