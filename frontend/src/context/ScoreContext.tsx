63d4cbb3544c9c2bf32bbf8fb2d2b47edca54b19
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2026-01-04 17:47:30 +0530

    Remove api reqs

--- frontend/src/context/ScoreContext.tsx
+++ frontend/src/context/ScoreContext.tsx
@@ -1,25 +1,6 @@
 import { createContext } from "react";
-import instance from "../utils/api";
-
-const initialScore = 0;
-
-async function getScore(): Promise<number | null> {
-  const playerToken = localStorage.getItem("player-token");
-  if (playerToken) {
-    const response = await instance.get(`/player`, {
-      headers: {
-        Authorization: `Bearer ${playerToken}`,
-      },
-    });
-    const { player } = response.data;
-    return player.score;
-  }
-  return null;
-}
-
-export const storedScore = await getScore();
 
 export const ScoreContext = createContext({
-  score: initialScore,
+  score: 0,
   incrementScore: () => {},
 });
